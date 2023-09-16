6dd89d5917cc03be278b2e7509e644907756a2b4
Yuriy Bezsonov <bezsonov@amazon.de>
2023-09-16 14:02:41 +0200

    update deploy to Cloud9 without WS

--- labs/unicorn-store/infrastructure/scripts/00-deploy-all.sh
+++ labs/unicorn-store/infrastructure/scripts/00-deploy-all.sh
@@ -4,6 +4,12 @@ echo $(date '+%Y.%m.%d %H:%M:%S')
 start_time=`date +%s`
 ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/timeprint.sh "Started" $start_time
 
+source ~/.bashrc
+aws cloud9 update-environment  --environment-id $C9_PID --managed-credentials-action DISABLE >/dev/null 2>&1
+rm -vf ${HOME}/.aws/credentials
+
+aws sts get-caller-identity --query Arn | grep java-on-aws-workshop-admin -q && echo "IAM role is valid" || echo "IAM role is NOT valid"
+
 ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/10-deploy-ci.sh
 ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/11-deploy-copilot.sh
 ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/12-deploy-ecs.sh
