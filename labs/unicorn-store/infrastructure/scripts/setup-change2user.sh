096195b1ab97744cf9e581bad7da7ab81eddcbb7
Yuriy Bezsonov <bezsonov@amazon.de>
2023-09-20 11:18:34 +0200

    change instance profile

--- labs/unicorn-store/infrastructure/scripts/setup-change2user.sh
+++ labs/unicorn-store/infrastructure/scripts/setup-change2user.sh
@@ -6,7 +6,12 @@ export INSTANCE_ID=$(wget -q -O - http://169.254.169.254/latest/dynamic/instance
 echo INSTANCE_ID=$INSTANCE_ID
 
 aws ec2 describe-iam-instance-profile-associations --query "IamInstanceProfileAssociations[?InstanceId=='$INSTANCE_ID'][IamInstanceProfile.Arn]" --output text
+export ASSOCIATION_ID=$(aws ec2 describe-iam-instance-profile-associations --query "IamInstanceProfileAssociations[?InstanceId=='$INSTANCE_ID'][AssociationId]" --output text)
+echo ASSOCIATION_ID=$ASSOCIATION_ID
+
+aws ec2 disassociate-iam-instance-profile --association-id $ASSOCIATION_ID
 aws ec2 associate-iam-instance-profile --iam-instance-profile Arn=arn:aws:iam::$ACCOUNT_ID:instance-profile/java-on-aws-workshop-user,Name=java-on-aws-workshop-user --instance-id $INSTANCE_ID
+
 aws ec2 describe-iam-instance-profile-associations --query "IamInstanceProfileAssociations[?InstanceId=='$INSTANCE_ID'][IamInstanceProfile.Arn]" --output text
 
 # export C9ID=$(aws cloud9 list-environments --query 'environmentIds[0]' --output text)
