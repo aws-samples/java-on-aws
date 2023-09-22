260af2305ecd06b0389b8c85fdd25d54aa3e2c4e
Yuriy Bezsonov <bezsonov@amazon.de>
2023-09-22 21:35:53 +0200

    iam

--- labs/unicorn-store/infrastructure/scripts/00-deploy-all.sh
+++ labs/unicorn-store/infrastructure/scripts/00-deploy-all.sh
@@ -8,11 +8,13 @@ source ~/.bashrc
 aws cloud9 update-environment  --environment-id $C9_PID --managed-credentials-action DISABLE >/dev/null 2>&1
 rm -vf ${HOME}/.aws/credentials
 
-aws sts get-caller-identity --query Arn | grep java-on-aws-workshop-user -q && echo "IAM role is valid" || echo "IAM role is NOT valid"
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+aws sts get-caller-identity --query Arn | grep java-on-aws-workshop-admin -q && echo "IAM role is valid" || printf "${RED}IAM role is NOT valid. To execute the scripts please switch to java-on-aws-workshop-admin profile$n"
 
 ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/10-deploy-ci.sh
 ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/11-deploy-ecs.sh
-# ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/12-deploy-copilot.sh
+~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/12-deploy-copilot.sh
 ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/20-deploy-eks.sh
 ~/environment/java-on-aws/labs/unicorn-store/infrastructure/scripts/deploy-gitops.sh
 
