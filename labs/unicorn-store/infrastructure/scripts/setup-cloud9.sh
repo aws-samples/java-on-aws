6e1df3f7439974b1309efcf846b290580932a251
Dennis Kieselhorst <mail@dekies.de>
2023-09-27 14:52:06 +0200

    update Maven version

--- labs/unicorn-store/infrastructure/scripts/setup-cloud9.sh
+++ labs/unicorn-store/infrastructure/scripts/setup-cloud9.sh
@@ -36,7 +36,7 @@ rm awscliv2.zip
 cd /tmp
 
 ## Install Maven
-export MVN_VERSION=3.9.2
+export MVN_VERSION=3.9.4
 export MVN_FOLDERNAME=apache-maven-${MVN_VERSION}
 export MVN_FILENAME=apache-maven-${MVN_VERSION}-bin.tar.gz
 curl -4 -L https://archive.apache.org/dist/maven/maven-3/${MVN_VERSION}/binaries/${MVN_FILENAME} | tar -xvz
