578a602ebc0e05df2a753e5174f28c9470f9fd64
Yuriy Bezsonov <bezsonov@amazon.de>
2023-09-28 16:44:12 +0200

    add eks deploy script

--- labs/unicorn-store/infrastructure/scripts/deploy-eks-eksctl.sh
+++ labs/unicorn-store/infrastructure/scripts/deploy-eks-eksctl.sh
@@ -5,6 +5,8 @@ start_time=`date +%s`
 
 cd ~/environment
 
+export CLUSTER_NAME=unicorn-store
+
 echo Get the existing VPC and Subnet IDs to inform EKS where to create the new cluster
 export UNICORN_VPC_ID=$(aws cloudformation describe-stacks --stack-name UnicornStoreVpc --query 'Stacks[0].Outputs[?OutputKey==`idUnicornStoreVPC`].OutputValue' --output text)
 export UNICORN_SUBNET_PRIVATE_1=$(aws ec2 describe-subnets \
@@ -17,14 +19,14 @@ export UNICORN_SUBNET_PUBLIC_2=$(aws ec2 describe-subnets \
 --filters "Name=vpc-id,Values=$UNICORN_VPC_ID" "Name=tag:Name,Values=UnicornStoreVpc/UnicornVpc/PublicSubnet2" --query 'Subnets[0].SubnetId' --output text)
 
 aws ec2 create-tags --resources $UNICORN_SUBNET_PRIVATE_1 $UNICORN_SUBNET_PRIVATE_2 \
---tags Key=kubernetes.io/cluster/unicorn-store,Value=shared Key=kubernetes.io/role/internal-elb,Value=1
+--tags Key=kubernetes.io/cluster/$CLUSTER_NAME,Value=shared Key=kubernetes.io/role/internal-elb,Value=1
 
 aws ec2 create-tags --resources $UNICORN_SUBNET_PUBLIC_1 $UNICORN_SUBNET_PUBLIC_2 \
---tags Key=kubernetes.io/cluster/unicorn-store,Value=shared Key=kubernetes.io/role/elb,Value=1
+--tags Key=kubernetes.io/cluster/$CLUSTER_NAME,Value=shared Key=kubernetes.io/role/elb,Value=1
 
 echo Create the cluster with eksctl
 eksctl create cluster \
---name unicorn-store \
+--name $CLUSTER_NAME \
 --version 1.27 --region $AWS_REGION \
 --nodegroup-name managed-node-group-x64 --managed --node-type m5.xlarge --nodes 2 --nodes-min 2 --nodes-max 4 \
 --with-oidc --full-ecr-access --alb-ingress-access \
@@ -32,8 +34,16 @@ eksctl create cluster \
 --vpc-public-subnets $UNICORN_SUBNET_PUBLIC_1,$UNICORN_SUBNET_PUBLIC_2
 
 echo Add the Participant IAM role to the list of the EKS cluster administrators to get access from the AWS Console..
-eksctl create iamidentitymapping --cluster unicorn-store --region=$AWS_REGION \
+eksctl create iamidentitymapping --cluster $CLUSTER_NAME --region=$AWS_REGION \
     --arn arn:aws:iam::$ACCOUNT_ID:role/WSParticipantRole --username admin --group system:masters \
+    --no-duplicate-arns
+
+eksctl create iamidentitymapping --cluster $CLUSTER_NAME --region=$AWS_REGION \
+    --arn arn:aws:iam::$ACCOUNT_ID:role/java-on-aws-workshop-user --username admin --group system:masters \
+    --no-duplicate-arns
+
+eksctl create iamidentitymapping --cluster $CLUSTER_NAME --region=$AWS_REGION \
+    --arn arn:aws:iam::$ACCOUNT_ID:role/java-on-aws-workshop-admin --username admin --group system:masters \
     --no-duplicate-arns
 
 echo Create a Kubernetes namespace for the application:
@@ -78,7 +88,7 @@ EOF
 aws iam create-policy --policy-name unicorn-eks-service-account-policy --policy-document file://service-account-policy.json
 
 echo Create a Kubernetes Service Account with a reference to the previous created IAM policy
-eksctl create iamserviceaccount --cluster=unicorn-store --name=unicorn-store-spring --namespace=unicorn-store-spring \
+eksctl create iamserviceaccount --cluster=$CLUSTER_NAME --name=unicorn-store-spring --namespace=unicorn-store-spring \
    --attach-policy-arn=$(aws iam list-policies --query 'Policies[?PolicyName==`unicorn-eks-service-account-policy`].Arn' --output text) --approve --region=$AWS_REGION
 rm service-account-policy.json
 
@@ -130,6 +140,8 @@ spec:
         key: unicornstore-db-secret
         property: password
 EOF
+
+aws eks --region $AWS_REGION update-kubeconfig --name unicorn-store
 
 echo $(date '+%Y.%m.%d %H:%M:%S')
 
