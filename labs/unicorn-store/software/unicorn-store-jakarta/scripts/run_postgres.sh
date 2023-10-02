ab24d111d2a6d9f0af81610bfadcfab425f9cda8
Yuriy Bezsonov <bezsonov@amazon.de>
2023-10-02 17:38:01 +0200

    quarkus scripts

--- labs/unicorn-store/software/unicorn-store-jakarta/scripts/run_postgres.sh
+++ labs/unicorn-store/software/unicorn-store-jakarta/scripts/run_postgres.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 5432:5432 -e POSTGRES_DB=unicorns -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres --name postgres postgres:13.11
+docker run --rm  --network host -p 5432:5432 -e POSTGRES_DB=unicorns -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres --name postgres postgres:13.11
--- labs/unicorn-store/software/unicorn-store-jakarta/scripts/run_quarkus.sh
+++ labs/unicorn-store/software/unicorn-store-jakarta/scripts/run_quarkus.sh
@@ -1,10 +1,12 @@
+docker build -t unicorn-store-quarkus:latest .
+
 export QUARKUS_DATASOURCE_JDBC_URL="jdbc:postgresql://localhost:5432/unicorns"
 export QUARKUS_DATASOURCE_PASSWORD="postgres"
 export QUARKUS_DATASOURCE_USERNAME="postgres"
 
-mvn quarkus:dev
+docker run --network host -p 8080:8080 unicorn-store-quarkus:latest -e QUARKUS_DATASOURCE_JDBC_URL=$QUARKUS_DATASOURCE_JDBC_URL -e QUARKUS_DATASOURCE_PASSWORD=$QUARKUS_DATASOURCE_PASSWORD -e QUARKUS_DATASOURCE_USERNAME=$QUARKUS_DATASOURCE_USERNAME
+
+# mvn quarkus:dev
 
 # mvn clean package
 # java -jar ./target/quarkus-app/quarkus-run.jar
-
-# docker build --no-cache -t unicorn-store-quarkus:latest .
