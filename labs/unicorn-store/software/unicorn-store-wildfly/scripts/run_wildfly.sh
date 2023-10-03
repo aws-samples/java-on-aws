34bf2fc31dc08dfd297ade7d10c19b3b79c3e498
Yuriy Bezsonov <bezsonov@amazon.de>
2023-10-03 19:47:26 +0200

    working wildfly

--- labs/unicorn-store/software/unicorn-store-wildfly/scripts/run_wildfly.sh
+++ labs/unicorn-store/software/unicorn-store-wildfly/scripts/run_wildfly.sh
@@ -1,8 +1,10 @@
+docker build --no-cache -t unicorn-store-wildfly:latest . && docker compose up && docker rm $(docker ps -aq)
+
 # docker build --no-cache -t unicorn-store:latest .
 
-docker run --rm -p 8080:8080 -p 9990:9990 --name wildfly \
-    -it unicorn-store:latest \
-    /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+# docker run --rm -p 8080:8080 -p 9990:9990 --name wildfly \
+#     -it unicorn-store:latest \
+#     /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 
 # mvn wildfly:execute-commands
 # mvn clean package wildfly:deploy
