08f7f751eb97999e5d8645177fe89939dfe6956e
Yuriy Bezsonov <bezsonov@amazon.de>
2023-10-11 20:15:48 +0100

    clean up

--- labs/unicorn-store/software/old/unicorn-store-javax/scripts/run_postgres.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -p 5432:5432 -e POSTGRES_DB=unicorn-store -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres --name unicorn-store postgres:13.11
--- labs/unicorn-store/software/old/unicorn-store-javax/scripts/run_wildfly.sh
+++
@@ -1,2 +0,0 @@
-mvn clean package
-java -jar target/wildfly-bootable.jar --deployment=target/store-jee.war -b=0.0.0.0
