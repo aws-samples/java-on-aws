4e691a7c12f505c6246dfa4e97ca76f3d4427da3
Yuriy Bezsonov <bezsonov@amazon.de>
2023-12-26 09:56:22 +0100

    remove unused imports

--- labs/unicorn-store/software/unicorn-store-spring/src/test/java/com/unicorn/store/UnicornControllerTest.java
+++ labs/unicorn-store/software/unicorn-store-spring/src/test/java/com/unicorn/store/UnicornControllerTest.java
@@ -16,13 +16,9 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import com.unicorn.store.model.Unicorn;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.TestPropertySource;
-
-import com.unicorn.store.model.Unicorn;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
