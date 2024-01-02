03af4675c9e008d73c72cbf27cf7cfa44eb311a0
Yuriy Bezsonov <bezsonov@amazon.de>
2024-01-02 11:01:52 +0100

    Update javax/jakarta artifact-ids

---
+++ labs/unicorn-store/software/unicorn-store-javax/scripts/run_javax.sh
@@ -0,0 +1,5 @@
+# cd unicorn-store-javax
+
+docker build --no-cache -f wildfly/Dockerfile -t unicorn-store-javax:latest .
+docker compose -f wildfly/docker-compose.yml up
+docker rm $(docker ps -a | grep "javax" | awk '{print $1}')
--- labs/unicorn-store/software/unicorn-store-javax/scripts/run_wildfly.sh
+++
@@ -1,5 +0,0 @@
-# cd unicorn-store-javax
-
-docker build --no-cache -f wildfly/Dockerfile -t unicorn-store-wildfly:latest .
-docker compose -f wildfly/docker-compose.yml up
-docker rm $(docker ps -a | grep "wildfly" | awk '{print $1}')
