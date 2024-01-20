25eb87bc5ab37b0f58d4bd6c1b79c0b028d55526
Yagr Xu <yagrxu@gmail.com>
2024-01-20 23:48:16 +0800

    add webconfig file change in script

--- labs/unicorn-store/software/scripts/enable-otel-manual.sh
+++ labs/unicorn-store/software/scripts/enable-otel-manual.sh
@@ -5,3 +5,4 @@ mv ./src/main/java/com/unicorn/store/otel/ApplicationFilter.java.opt ./src/main/
 mv ./src/main/java/com/unicorn/store/otel/MetricEmitter.java.opt ./src/main/java/com/unicorn/store/otel/MetricEmitter.java
 mv ./src/main/java/com/unicorn/store/otel/PseudoRandomNumberGenerator.java.opt ./src/main/java/com/unicorn/store/otel/PseudoRandomNumberGenerator.java
 mv ./src/main/java/com/unicorn/store/otel/RandomNumberGenerator.java.opt ./src/main/java/com/unicorn/store/otel/RandomNumberGenerator.java
+mv  ~/environment/unicorn-store-spring/src/main/java/com/unicorn/store/config/WebConfig.java.opt ~/environment/unicorn-store-spring/src/main/java/com/unicorn/store/config/WebConfig.java
