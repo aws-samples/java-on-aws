996e9e2d1a36f783f79a8b9b1db6ee1622a1061d
Yagr Xu <yagrxu@gmail.com>
2024-02-07 13:00:41 +0800

    fix enable manual otel script

--- labs/unicorn-store/software/scripts/enable-otel-manual.sh
+++ labs/unicorn-store/software/scripts/enable-otel-manual.sh
@@ -1,7 +1,6 @@
-
-
 cd ~/environment/unicorn-store-spring
 mv ./src/main/java/com/unicorn/store/otel/ApplicationFilter.java.opt ./src/main/java/com/unicorn/store/otel/ApplicationFilter.java
+mv ./src/main/java/com/unicorn/store/otel/TracingRequestInterceptor.java.opt ./src/main/java/com/unicorn/store/otel/TracingRequestInterceptor.java
 mv ./src/main/java/com/unicorn/store/otel/MetricEmitter.java.opt ./src/main/java/com/unicorn/store/otel/MetricEmitter.java
 mv ./src/main/java/com/unicorn/store/otel/PseudoRandomNumberGenerator.java.opt ./src/main/java/com/unicorn/store/otel/PseudoRandomNumberGenerator.java
 mv ./src/main/java/com/unicorn/store/otel/RandomNumberGenerator.java.opt ./src/main/java/com/unicorn/store/otel/RandomNumberGenerator.java
