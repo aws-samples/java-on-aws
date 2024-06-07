7da6d4e85fd3609fdd2272b4284db1d5c750c79c
Dennis Kieselhorst <mail@dekies.de>
2024-06-07 10:00:34 +0200

    K8s 1.30

--- labs/unicorn-store/infrastructure/scripts/setup-ide.sh
+++ labs/unicorn-store/infrastructure/scripts/setup-ide.sh
@@ -123,7 +123,7 @@ eksctl version
 
 ## kubectl
 # https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
