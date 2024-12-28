4c7736333e615cc70123033058544d9ee811fc5c
Yuriy Bezsonov <bezsonov@amazon.de>
2024-12-28 13:16:04 +0100

    update scripts

--- infrastructure/scripts/cleanup/ws-cleanup-eks.sh
+++ infrastructure/scripts/cleanup/ws-cleanup-eks.sh
@@ -10,3 +10,24 @@ kubectl delete service $APP_NAME -n $APP_NAME
 kubectl delete deployment $APP_NAME -n $APP_NAME --cascade=foreground
 kubectl delete sa $APP_NAME -n $APP_NAME
 kubectl delete namespace $APP_NAME
+
+kubectl delete ingressclass alb
+kubectl delete nodepool dedicated
+kubectl delete nodepool nodes-arm64
+
+helm uninstall external-secrets -n external-secrets
+
+# List all Pod Identity associations in the cluster
+ASSOCIATIONS=$(aws eks list-pod-identity-associations \
+    --cluster-name unicorn-store \
+    --query 'associations[].associationId' \
+    --output text) && echo $ASSOCIATIONS
+
+# Delete each association
+for ASSOCIATION_ID in $ASSOCIATIONS; do
+    echo "Deleting association: $ASSOCIATION_ID"
+    aws eks delete-pod-identity-association \
+        --cluster-name unicorn-store \
+        --association-id $ASSOCIATION_ID \
+        --no-cli-pager
+done
