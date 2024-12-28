878f102cbbb499a16a0bc83c66f586008b3ba1f4
Yuriy Bezsonov <bezsonov@amazon.de>
2024-12-28 17:09:40 +0100

    create vpc connector

--- infrastructure/scripts/cleanup/ws-cleanup-apprunner.sh
+++ infrastructure/scripts/cleanup/ws-cleanup-apprunner.sh
@@ -5,8 +5,8 @@ if [[ "$APPRUNNER_ARN" != "" ]]
         while [[ $(aws apprunner list-services --query 'ServiceSummaryList[?ServiceName==`unicorn-store-spring`].ServiceArn' --output text) == $APPRUNNER_ARN ]] && [[ $(aws apprunner list-operations --service-arn $APPRUNNER_ARN) != "SUCCEEDED" ]]; do echo "Service not yet deleted ..." &&  sleep 10; done
 fi
 
-# Get the VPC connector ARN
-VPC_CONNECTOR_ARN=$(aws apprunner list-vpc-connectors --query 'VpcConnectors[?VpcConnectorName==`unicornstore-vpc-connector`].VpcConnectorArn' --output text)
+# # Get the VPC connector ARN
+# VPC_CONNECTOR_ARN=$(aws apprunner list-vpc-connectors --query 'VpcConnectors[?VpcConnectorName==`unicornstore-vpc-connector`].VpcConnectorArn' --output text)
 
-# Delete the VPC connector
-aws apprunner delete-vpc-connector --vpc-connector-arn $VPC_CONNECTOR_ARN --no-cli-pager
+# # Delete the VPC connector
+# aws apprunner delete-vpc-connector --vpc-connector-arn $VPC_CONNECTOR_ARN --no-cli-pager
