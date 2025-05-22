7045ddd1926de5e771a4eeb348f05c196c53ebe8
Maximilian Schellhorn <mxschell@amazon.de>
2025-05-22 15:09:24 +0200

    feat: added spring cloud function sample

---
+++ samples/spring-cloud-function-demo/src/test/java/com/amazonaws/demo/FunctionCatalogTest.java
@@ -0,0 +1,53 @@
+package com.amazonaws.demo;
+
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.config.RoutingFunction;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class FunctionCatalogTest {
+
+    @Autowired
+    private FunctionCatalog functionCatalog;
+
+    @Test
+    public void testFunction() {
+        Function<String, String> functionDefinition = functionCatalog.lookup("upperCase");
+        var result = functionDefinition.apply("spring");
+
+        assertEquals("SPRING", result);
+    }
+
+    //Routing Function Test - Defined in application.properties
+    //spring.cloud.function.definition=reverse
+    @Test
+    public void testFunctionRouting() {
+        Function<String, String> functionDefinition = functionCatalog.lookup(RoutingFunction.FUNCTION_NAME);
+        var result = functionDefinition.apply("spring");
+
+        assertEquals("gnirps", result);
+    }
+
+    @Test
+    public void testHandleSQSMessages() {
+        // Get function from catalog
+        Consumer<SQSEvent> function = functionCatalog.lookup(Function.class, "asyncProcessor");
+        assertNotNull(function);
+
+        var sqsMessage = new SQSEvent();
+        sqsMessage.setRecords(List.of(new SQSEvent.SQSMessage()));
+
+        // Create test data and invoke function
+        function.accept(sqsMessage);
+    }
+}
