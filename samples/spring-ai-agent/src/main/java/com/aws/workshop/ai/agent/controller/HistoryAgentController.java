fbd3bf5347da26355c7de5d916b0a81ce4a01316
ashakirin <2254222+ashakirin@users.noreply.github.com>
2025-05-27 16:50:45 +0200

    Added controller with memory and sample requests (#707)

--- samples/spring-ai-agent/src/main/java/com/aws/workshop/ai/agent/controller/HistoryAgentController.java
+++ samples/spring-ai-agent/src/main/java/com/aws/workshop/ai/agent/controller/HistoryAgentController.java
@@ -1,6 +1,9 @@
 package com.aws.workshop.ai.agent.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.web.bind.annotation.*;
 import software.amazon.awssdk.utils.StringInputStream;
@@ -9,19 +12,27 @@ import java.io.InputStream;
 import java.util.Objects;
 
 @RestController
-public class AgentController {
+@RequestMapping("/memory")
+public class HistoryAgentController {
     private ChatClient chatClient;
     private final ChatClient.Builder chatClientBuilder;
+    private final PromptChatMemoryAdvisor promptChatMemoryAdvisor;
 
-    public AgentController(ChatClient chatClient, ChatClient.Builder chatClientBuilder) {
+    public HistoryAgentController(ChatClient chatClient, ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClient;
         this.chatClientBuilder = chatClientBuilder;
+        var memory =
+                MessageWindowChatMemory.builder()
+                        .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                        .build();
+        this.promptChatMemoryAdvisor = PromptChatMemoryAdvisor.builder(memory).build();
     }
 
     @PostMapping("/chat")
     public String chat(@RequestBody String prompt) {
         return chatClient
                 .prompt()
+                .advisors(promptChatMemoryAdvisor)
                 .user(prompt)
                 .call()
                 .content();
@@ -31,6 +42,7 @@ public class AgentController {
     public InputStream chatStream(@RequestBody String prompt) {
         return new StringInputStream(Objects.requireNonNull(chatClient
                 .prompt()
+                .advisors(promptChatMemoryAdvisor)
                 .user(prompt)
                 .call()
                 .content()));
