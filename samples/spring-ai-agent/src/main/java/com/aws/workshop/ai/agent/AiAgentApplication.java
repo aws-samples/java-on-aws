16dc39778979b55055261bbd620b3ebc73b7e096
ashakirin <2254222+ashakirin@users.noreply.github.com>
2025-05-27 10:47:21 +0200

    Initial commit for spring-ai agent (#706)

---
+++ samples/spring-ai-agent/src/main/java/com/aws/workshop/ai/agent/AiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.aws.workshop.ai.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAgentApplication.class, args);
+    }
+
+}
