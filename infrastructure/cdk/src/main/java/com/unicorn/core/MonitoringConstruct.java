ac09ac5327a7f9a918937e5aa0403cbd05283bbe
Sascha Moellering <smoell@amazon.de>
2025-05-31 09:13:58 +0200

    Removed AMG and AMP

--- infrastructure/cdk/src/main/java/com/unicorn/core/MonitoringConstruct.java
+++ infrastructure/cdk/src/main/java/com/unicorn/core/MonitoringConstruct.java
@@ -1,13 +1,9 @@
 package com.unicorn.core;
 
 import software.amazon.awscdk.CfnOutput;
-import software.amazon.awscdk.Duration;
-import software.amazon.awscdk.customresources.Provider;
 import software.amazon.awscdk.services.ec2.IVpc;
 import software.amazon.awscdk.services.iam.*;
 import software.amazon.awscdk.services.lambda.*;
-import software.amazon.awscdk.services.secretsmanager.Secret;
-import software.amazon.awscdk.services.secretsmanager.SecretStringGenerator;
 import software.amazon.awscdk.services.sns.Topic;
 import software.amazon.awscdk.services.sns.TopicPolicy;
 import software.amazon.awscdk.services.sns.subscriptions.LambdaSubscription;
@@ -21,7 +17,6 @@ public class MonitoringConstruct extends Construct {
 
     private final Topic alarmTopic;
     private String prometheusInternalUrl;
-    private String grafanaUrl;
 
     public MonitoringConstruct(Construct scope, String id, IVpc vpc, CfnCluster eksCluster, Function alertHandlerLambda) {
         super(scope, id);
@@ -47,52 +42,10 @@ public class MonitoringConstruct extends Construct {
 
         prometheusInternalUrl = "http://prometheus-server.monitoring.svc.cluster.local";
 
-        createGrafanaUrlOutput();
-
         CfnOutput.Builder.create(this, "PrometheusInternalUrl")
                 .description("Prometheus internal service URL (for ECS use)")
                 .value(prometheusInternalUrl)
                 .exportName("PrometheusInternalUrl")
-                .build();
-    }
-
-    private void createGrafanaUrlOutput() {
-        Function fetchGrafanaDnsLambda = Function.Builder.create(this, "FetchGrafanaDns")
-                .runtime(software.amazon.awscdk.services.lambda.Runtime.PYTHON_3_11)
-                .handler("index.handler")
-                .timeout(Duration.minutes(1))
-                .code(Code.fromInline(
-                        "import boto3\n" +
-                                "import os\n\n" +
-                                "def handler(event, context):\n" +
-                                "    elb = boto3.client('elbv2')\n" +
-                                "    response = elb.describe_load_balancers()\n" +
-                                "    for lb in response['LoadBalancers']:\n" +
-                                "        if 'grafana' in lb['LoadBalancerName'] and lb['Scheme'] == 'internet-facing':\n" +
-                                "            return { 'PhysicalResourceId': lb['DNSName'], 'Data': { 'GrafanaUrl': 'http://' + lb['DNSName'] } }\n" +
-                                "    raise Exception('Grafana Load Balancer not found')"
-                ))
-                .initialPolicy(List.of(
-                        PolicyStatement.Builder.create()
-                                .effect(Effect.ALLOW)
-                                .actions(List.of("elasticloadbalancing:DescribeLoadBalancers"))
-                                .resources(List.of("*"))
-                                .build()
-                ))
-                .build();
-
-        Provider provider = Provider.Builder.create(this, "GrafanaUrlProvider")
-                .onEventHandler(fetchGrafanaDnsLambda)
-                .build();
-
-        software.amazon.awscdk.CustomResource resource = software.amazon.awscdk.CustomResource.Builder.create(this, "GrafanaUrlCustom")
-                .serviceToken(provider.getServiceToken())
-                .build();
-
-        CfnOutput.Builder.create(this, "GrafanaDashboardUrl")
-                .description("Grafana public Web UI")
-                .value(resource.getAttString("GrafanaUrl"))
-                .exportName("GrafanaUrl")
                 .build();
     }
 
@@ -102,9 +55,5 @@ public class MonitoringConstruct extends Construct {
 
     public String getPrometheusInternalUrl() {
         return prometheusInternalUrl;
-    }
-
-    public String getGrafanaUrl() {
-        return grafanaUrl;
     }
 }
