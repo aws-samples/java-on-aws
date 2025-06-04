686dc3c6c8191b7d1706bcf98b4cb1a8f7c7fa49
ashakirin <andrei.shakirin@gmail.com>
2025-06-04 12:14:39 +0200

    Added skeleton for intergation test using testcontainers

--- samples/spring-ai-agent/src/test/java/com/aws/workshop/ai/agent/AiAgentApplicationTests.java
+++ samples/spring-ai-agent/src/test/java/com/aws/workshop/ai/agent/AiAgentApplicationTests.java
@@ -2,15 +2,19 @@ package com.aws.workshop.ai.agent;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.sql.init.mode=never",
-        "spring.jpa.hibernate.ddl-auto=none",
-        "spring.liquibase.enabled=false"
-})
 class AiAgentApplicationTests {
+    @Container
+    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(
+            DockerImageName.parse("pgvector/pgvector:pg16")
+                    .asCompatibleSubstituteFor("postgres"))
+            .withDatabaseName("ai-agent-db")
+            .withUsername("chatuser")
+            .withPassword("chatpass");
 
     @Test
     void contextLoads() {
