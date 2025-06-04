fa08ededcd53bcecd83ea98d149e6b3d257e3fab
Lefteris Karageorgiou <lefkarag@amazon.com>
2025-06-04 10:52:09 +0300

    Working version of RAG pg vector controller

--- samples/spring-ai-agent/src/main/java/com/aws/workshop/ai/agent/controller/RagPgVectorAgentController.java
+++ samples/spring-ai-agent/src/main/java/com/aws/workshop/ai/agent/controller/RagPgVectorAgentController.java
@@ -3,7 +3,7 @@ package com.aws.workshop.ai.agent.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.prompt.ChatOptions;
-import org.springframework.ai.observation.conventions.VectorStoreProvider;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,29 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 import software.amazon.awssdk.utils.StringInputStream;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @RestController
 @RequestMapping("/rag-pgvector")
 public class RagPgVectorAgentController {
     private ChatClient chatClient;
     private final ChatClient.Builder chatClientBuilder;
+    private final VectorStore vectorStore;
     private final QuestionAnswerAdvisor ragAdvisor;
 
     public RagPgVectorAgentController(ChatClient chatClient, VectorStore vectorStore, ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClient;
         this.chatClientBuilder = chatClientBuilder;
+        this.vectorStore = vectorStore;
         ragAdvisor = QuestionAnswerAdvisor.builder(vectorStore).build();
     }
 
     @PostMapping("/chat")
     public String chat(@RequestBody String prompt) {
-        return chatClient
+        var content = chatClient
                 .prompt()
                 .advisors(ragAdvisor)
                 .user(prompt)
                 .call()
                 .content();
+
+        vectorStore.add(List.of(new Document(content)));
+
+        return content;
     }
 
     @PostMapping("/chat/stream")
