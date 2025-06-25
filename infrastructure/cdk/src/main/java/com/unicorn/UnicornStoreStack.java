db6563933c945686312d6aebbe059b39d388c9ba
Sascha Moellering <smoell@amazon.de>
2025-06-25 09:09:45 +0200

    Added monitoring-setup

--- infrastructure/cdk/src/main/java/com/unicorn/UnicornStoreStack.java
+++ infrastructure/cdk/src/main/java/com/unicorn/UnicornStoreStack.java
@@ -30,11 +30,12 @@ public class UnicornStoreStack extends Stack {
         sudo -H -i -u ec2-user bash -c "~/java-on-aws/infrastructure/scripts/setup/ide.sh"
 
         echo '=== Additional Setup ==='
+        whoami
         echo '=== Checking directory ==='
-        ls -la ~/java-on-aws/infrastructure/scripts/setup/
+        sudo -H -i -u ec2-user bash -c "ls -la ~/java-on-aws/infrastructure/scripts/setup/"
         sudo -H -i -u ec2-user bash -c "~/java-on-aws/infrastructure/scripts/setup/app.sh"
         sudo -H -i -u ec2-user bash -c "~/java-on-aws/infrastructure/scripts/setup/eks.sh"
-        if ! test -f ~/java-on-aws/infrastructure/scripts/setup/monitoring.sh; then
+        if ! test -f /home/ec2-user/java-on-aws/infrastructure/scripts/setup/monitoring.sh; then
           echo "File monitoring.sh does not exists."
         fi
         sudo -H -i -u ec2-user bash -c "~/java-on-aws/infrastructure/scripts/setup/monitoring.sh"
