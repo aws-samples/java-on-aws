4e4db590c5c95a9c239689984fad1d7fc167325e
Yuriy Bezsonov <bezsonov@amazon.de>
2025-06-26 18:39:55 +0200

    disable zsh

--- infrastructure/scripts/setup/idp.sh
+++ infrastructure/scripts/setup/idp.sh
@@ -24,5 +24,5 @@ cd ~/tmp
 curl -sSL https://raw.githubusercontent.com/aws-samples/fleet-management-on-amazon-eks-workshop/refs/heads/riv24/hack/.zshrc -o .zshrc
 curl -sSL https://raw.githubusercontent.com/aws-samples/fleet-management-on-amazon-eks-workshop/refs/heads/riv24/hack/.p10k.zsh -o .p10k.zsh
 curl -sSL https://tinyurl.com/installBox | bash
-sudo usermod -s $(which zsh) $USER
-echo "exec zsh" >> ~/.bashrc
+# sudo usermod -s $(which zsh) $USER
+# echo "exec zsh" >> ~/.bashrc
